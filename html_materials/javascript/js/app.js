604326689d5076cda590b13e95ad39de6c083e20
mmoraleda <mmoraleda@smartvel.com>
2015-07-07 16:27:09 +0200

    Revert "add workshop html materials"
    This reverts commit 9fafa41ca835489c675033b78ed2f7e8bc0b178c.

--- html_materials/javascript/js/app.js
+++
@@ -1,8 +0,0 @@
- var app = angular.module("MusicApp",[]);
-
- app.config( ['$compileProvider',
-    function( $compileProvider )
-    {   
-        $compileProvider.aHrefSanitizationWhitelist(/^\s*(spotify):/);
-
-    }]);
--- html_materials/javascript/js/directives/discosList.js
+++
@@ -1,7 +0,0 @@
-app.directive("discosList", function(){
-  return {
-    restrict: "E",
-    scope:{
-      disco: "="
-    },
-    templateUrl: "js/templates/discosList.html"}; });  
