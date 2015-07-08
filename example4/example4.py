29fd4c4d61d796ffafb6c288e51bb2b2be1a4a98
Miguel M <mmoraleda@smartvel.com>
2015-07-08 10:02:20 +0200

    example4 step2

---
+++ example4/example4.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import structlog
+import requests
+from bs4 import BeautifulSoup
+
+logger = structlog.getLogger(__name__)
+
+# Example 4: There is a paginated web, loading the info with Angularjs(javascript).
+# 1.Make a get request and capture with curl, browser or similar.
+# 2.Analize the request.
+# 3.Use selenium to retrieve the web.
+# 4.Analize the retrieved information, find the next page button and use selenium to change the page clicking on it.
+# 5.Build the scraper to retrieve the 13 discs information.
+
+
+# Class with the fields of a disc
+class Disco():
+    def __init__(self, id='', title='', author='', link='', image='', canciones=[]):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.link = link
+        self.image = image
+        self.canciones = canciones
+
+if __name__ == "__main__":
+
+    discos = []
+    url = 'http://scrap.smartvel.net/javascript/example4.html'
+
+    # Rules
+    rules = {'discos': ('div', {'class': 'disco'}),
+             'id': ('span', {'class', 'id'}),
+             'title': ('h1', {'class', 'titulo'}),
+             'author': ('span', {'class', 'author'}),
+             'link': ('a', {'class', 'spotifylink'}),
+             'image': ('img',),
+             'canciones': ('span', {'class', 'cancion'}),
+             'pages': ('a', {'class', 'page'})
+             }
+
+    # Retrieve the page with GET REQUEST
+    page = requests.get(url)
+
+    logger.info("Spider collects {} discs ".format(page.text))
